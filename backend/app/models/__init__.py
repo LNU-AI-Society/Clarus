361d171d144a2538a466eec73a2296db06cf7d51
Bogdan Sorinescu <bogda@MacBook-Pro.local>
2026-01-19 18:11:54 +0100

    Tested inserts for GuidedAnswerDB and json format data storage

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -1,7 +1,7 @@
 from sqlmodel import SQLModel
 
 from app.models.chat import ChatMessageDB
-from app.models.guided import GuidedSessionDB
+from app.models.guided import GuidedSessionDB, GuidedAnswerDB
 from app.models.user import User
 
 # This file helps import all models so SQLModel knows about them when creating tables
