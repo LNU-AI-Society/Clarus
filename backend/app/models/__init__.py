2cf4a76ea73adedeb2dd3fefb4024f58de567e3c
Balazs Hevesi <bh222pv@student.lnu.se>
2026-01-22 18:31:06 +0100

    Remove python backend fully

--- backend/app/models/__init__.py
+++
@@ -1,5 +0,0 @@
-from sqlmodel import SQLModel
-
-from app.models.chat import ChatMessageDB
-from app.models.guided import GuidedSessionDB, GuidedAnswerDB
-# This file helps import all models so SQLModel knows about them when creating tables
